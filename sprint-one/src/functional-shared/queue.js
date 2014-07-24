7019b93578fde3fb67b58f20c1843a1b237c4a61
Jim OBrien <jimobrien930@gmail.com>
2014-07-24 08:50:33 -0700

    worked on queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,22 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  _.extend(someInstance, queueMethods);
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  push: function (val) {
+
+  },
+  pop: function () {
+
+  },
+  size: function () {
+
+  }
+};
 
 
 
