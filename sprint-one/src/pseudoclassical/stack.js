3511e5beee5468097ace514617c8e0b641380745
Jim OBrien <jimobrien930@gmail.com>
2014-07-24 08:46:18 -0700

    implemeneted pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -5,13 +5,17 @@ var Stack = function() {
 };
 
 Stack.prototype.push = function (value) {
-  
+  var key = this.size();
+  this.storage[key] = value;
 };
 
 Stack.prototype.pop = function () {
-  
+  var key = this.size() ? this.size() - 1 : 0;
+  var popped = this.storage[key];
+  delete this.storage[key];
+  return popped;
 };
 
 Stack.prototype.size = function () {
-  
+  return Object.keys(this.storage).length;
 };
